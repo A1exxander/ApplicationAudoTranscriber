8219d717025ad31b4a0caee103a41107d4913ed8
Alex <raposoalexander@gmail.com>
2024-12-12 02:39:07 -0500

    Modified model's absolute path to a relative path

--- src/main/java/ApplicationAudioTranscriber.java
+++ src/main/java/ApplicationAudioTranscriber.java
@@ -8,14 +8,14 @@ import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
 import org.vosk.LibVosk;
 import org.vosk.LogLevel;
 import org.vosk.Model;
-
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class ApplicationAudioTranscriber {
 
     public static void main(String[] args) throws NativeHookException, IOException {
         final int recordToggleKeyCode = (args.length == 0) ? NativeKeyEvent.VC_F2 : Utils.getVCCode(args[0]);
-        final String audioTranscriptionModelPath = "C:\\Users\\Alex\\IdeaProjects\\ApplicationAudioTranscriber\\src\\main\\java\\AudioTranscriber\\model\\vosk-model-small-en-us-0.15";
+        final String audioTranscriptionModelPath = Paths.get("src","main", "resources", "vosk-model-small-en-us-0.15").toAbsolutePath().toString();
         LibVosk.setLogLevel(LogLevel.WARNINGS);
         AudioKeyListener audioKeyListener = new AudioKeyListener(recordToggleKeyCode, new AudioRecorder(), new VoskAudioTranscriber(new Model(audioTranscriptionModelPath)));
         GlobalScreen.registerNativeHook();
@@ -24,7 +24,7 @@ public class ApplicationAudioTranscriber {
     }
 
     /*
-        TODO: 1. Minor refactorings
+        TODO: 1. Minor refactorings - Namely relative path, final fields, audio format being passed, etc
               2. Update to use system wide audio as input instead of microphone
               3. Dockerize application
               4. Create test cases & write unit tests
